4dfc394518894d8961ca39decc5758333349dab3
thony311 <antoinelefebvrepoupa@hotmail.com>
2023-04-11 10:03:11 -0400

    regex email
    les regex marche a moitier manque les 3 noms

--- Laravel/app/Http/Controllers/ComptesController.php
+++ Laravel/app/Http/Controllers/ComptesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Compte;
+use App\Http\Requests\ComptesAdminRequest;
 
 class ComptesController extends Controller
 {
@@ -87,16 +88,22 @@ class ComptesController extends Controller
         return View('comptes.createAdmin');
     }
 
-    public function storeAdmin(Request $request)
+    public function storeAdmin(ComptesAdminRequest $request)
     {
+        // sauvegarde d'admins dedans la base de données
         try{
-            
-            $motDePasse = substr($request->get('nom'),0,3) . substr($request->get('prenom'),0,3);
+            // création d'un mot de passe temporaire a partir ddes trois premières lettres du prenom nom et 123
+            $motDePasse = substr($request->get('nom'),0,3) . substr($request->get('prenom'),0,3 . "123");
+            // prend toute les données rentrez dedans le form
             $compte = new Compte($request->all());
+            // cryption du mot de passe
             $compte->motDePasse = sha1($motDePasse);
+            // ajout de type admin
             $compte->typeCompte = "Admin";
+            // sauvegarde
             $compte->save();
-            return redirect()->route('Comptes.index');
+            // redirection
+            return redirect()->route('Comptes.createAdmin');
         }
         catch(Throwable $e){
             Log::debug($e);
